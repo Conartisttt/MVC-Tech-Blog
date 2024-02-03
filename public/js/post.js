3b0f618a48aa21f9762d00a15a8e2f677402eee9
Conartisttt <connerwmar@gmail.com>
2024-02-03 17:39:55 -0500

    ability to update post

--- public/js/post.js
+++ public/js/post.js
@@ -22,6 +22,30 @@ const newFormHandler = async (event) => {
   }
 };
 
+const updatePostHandler = async (event) => {
+  event.preventDefault();
+  if (event.target.hasAttribute('data-update-id')) {
+    const id = event.target.getAttribute('data-update-id');
+
+    const title = document.getElementById('updatedPostTitle').value.trim();
+    const content = document.getElementById('updatedPostContent').value.trim();
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to update project');
+    }
+  }
+};
+
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
@@ -47,18 +71,65 @@ const cancelBtnHandler = (event) => {
   event.preventDefault();
   const createPostBtnDiv = document.getElementById('createPostBtnDiv').classList.remove('hide');
   const createPostDiv = document.getElementById('createPostDiv').classList.add('hide');
-  const titleInput = document.getElementById('project-name').value="";
-  const descriptionInput = document.getElementById('project-desc').value="";
+  const titleInput = document.getElementById('project-name').value = "";
+  const descriptionInput = document.getElementById('project-desc').value = "";
 }
 
-const toggleViewBtn = document.getElementById('createPostBtn');
-if (toggleViewBtn) {
-  toggleViewBtn.addEventListener('click', createPostView);
+const editBtnHandler = () => {
+  const editPostBtnDiv = document.getElementById('editPostBtnDiv').classList.add('hide');
+  const updateDeleteDiv = document.getElementById('updateDeleteDiv').classList.remove('hide');
 }
 
-const cancelUpdateBtn = document.getElementById('cancelUpdateBtn');
-if (cancelUpdateBtn) {
-  cancelUpdateBtn.addEventListener('click', cancelBtnHandler)
+const updateViewHandler = () => {
+  const updateForm = document.getElementById('updateForm');
+  updateForm.classList.remove('hide');
+  const currentPostDiv = document.getElementById('indivPost');
+  currentPostDiv.classList.add('hide');
+}
+
+const cancelUpdateHandler = (event) => {
+  event.preventDefault();
+  const updateForm = document.getElementById('updateForm');
+  updateForm.classList.add('hide');
+  const currentPostDiv = document.getElementById('indivPost');
+  currentPostDiv.classList.remove('hide');
+}
+
+const cancelEditHandler = () => {
+  const editPostBtnDiv = document.getElementById('editPostBtnDiv').classList.remove('hide');
+  const updateDeleteDiv = document.getElementById('updateDeleteDiv').classList.add('hide');
+}
+
+const toggleCreateBtn = document.getElementById('createPostBtn');
+if (toggleCreateBtn) {
+  toggleCreateBtn.addEventListener('click', createPostView);
+}
+
+const cancelCreateBtn = document.getElementById('cancelCreateBtn');
+if (cancelCreateBtn) {
+  cancelCreateBtn.addEventListener('click', cancelBtnHandler)
+}
+
+const toggleEditBtn = document.querySelectorAll('.toggleEdit');
+if (toggleEditBtn) {
+  for (let i = 0; i < toggleEditBtn.length; i++) {
+    toggleEditBtn[i].addEventListener('click', editBtnHandler)
+  }
+}
+
+const toggleUpdateBtn = document.getElementById('updatePostBtn');
+if (toggleUpdateBtn) {
+  toggleUpdateBtn.addEventListener('click', updateViewHandler)
+}
+
+const cancelEditBtn = document.getElementById('cancelEditBtn');
+if (cancelEditBtn) {
+  cancelEditBtn.addEventListener('click', cancelEditHandler)
+}
+
+const updatePost = document.getElementById('submitUpdateBtn');
+if (updatePost) {
+  updatePost.addEventListener('click', updatePostHandler)
 }
 
 const submitPost = document.querySelector('.new-project-form');
@@ -70,3 +141,4 @@ const deletePost = document.querySelector('.project-list');
 if (deletePost) {
   deletePost.addEventListener('click', delButtonHandler);
 }
+
